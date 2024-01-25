4cccfb839ac93ee3d3e12eb4e68b806fae007d68
Pratham Kumar <150330048+kumarpratham@users.noreply.github.com>
2024-01-26 02:29:03 +0530

    splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.coding.whatstheweather"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.coding.whatstheweather"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
     }
 }
 
