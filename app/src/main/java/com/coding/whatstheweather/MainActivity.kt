882febaa2c12619c9a4dab46805587df53ef2d54
Pratham Kumar <150330048+kumarpratham@users.noreply.github.com>
2024-01-27 01:56:16 +0530

    main UI

--- app/src/main/java/com/coding/whatstheweather/MainActivity.kt
+++ app/src/main/java/com/coding/whatstheweather/MainActivity.kt
@@ -3,6 +3,9 @@ package com.coding.whatstheweather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//  a01272aa0c5edd71776613ca8f18f261
+// https://api.openweathermap.org/data/2.5/weather?q=London&APPID=a01272aa0c5edd71776613ca8f18f261
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
